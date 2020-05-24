86be7b4821e486ff68ef2422659d183da2630f04
yesid davila <elmondoles@gmail.com>
2020-05-24 02:14:24 -0500

    + Modal + cards + navegacion stickyt

--- src/app/casos/casos.component.ts
+++ src/app/casos/casos.component.ts
@@ -7,6 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CasosComponent implements OnInit {
 
+  datos = [
+            {
+              foto: '../../assets/img/users/1-mujer.jpg',
+              nombre: 'Erika k.',
+              origen: 'Mocarí / Monteria',
+              placa: 'cbx-12 de B/quilla.',
+              destino: 'Planta Malambo / Barranquilla'
+            },
+            {
+              foto: '../../assets/img/users/3mujer.jpg',
+              nombre: 'Ivana M.',
+              origen: 'Mocarí / Monteria',
+              placa: 'cbx-12 de B/quilla.',
+              destino: 'Planta Malambo / Barranquilla'
+            },
+            {
+              foto: '../../assets/img/users/4mujer.jpg',
+              nombre: 'Ivana M.',
+              origen: 'Mocarí / Monteria',
+              placa: 'cbx-12 de B/quilla.',
+              destino: 'Planta Malambo / Barranquilla'
+            },
+            {
+              foto: '../../assets/img/users/5mujer.jpg',
+              nombre: 'Ivana M.',
+              origen: 'Mocarí / Monteria',
+              placa: 'cbx-12 de B/quilla.',
+              destino: 'Planta Malambo / Barranquilla'
+            }
+          ];
+
   constructor() { }
 
   ngOnInit(): void {
